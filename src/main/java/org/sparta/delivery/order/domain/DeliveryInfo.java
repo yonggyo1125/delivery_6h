8be56d88c8c1ea084a382428267dad50a315dde7
yonggyo1125 <yonggyo00@kakao.com>
2026-02-26 15:55:30 +0900

    .

--- src/main/java/org/sparta/delivery/order/domain/DeliveryInfo.java
+++
@@ -1,7 +0,0 @@
-package org.sparta.delivery.order.domain;
-
-public class DeliveryInfo {
-    private String address;
-    private String addressDetail;
-    private String memo;
-}
--- src/main/java/org/sparta/delivery/order/domain/OrderId.java
+++
@@ -1,7 +0,0 @@
-package org.sparta.delivery.order.domain;
-
-import java.util.UUID;
-
-public class OrderId {
-    private UUID id;
-}
