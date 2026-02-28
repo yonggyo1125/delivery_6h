4970ebb354808e0315803eaaf6bf91d37bec7374
yonggyo1125 <yonggyo00@kakao.com>
2026-02-28 18:21:47 +0900

    .

---
+++ src/main/java/org/sparta/delivery/store/application/product/CreateProductService.java
@@ -0,0 +1,15 @@
+package org.sparta.delivery.store.application.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CreateProductService {
+
+    public UUID create() {
+        return null;
+    }
+}
--- src/main/java/org/sparta/delivery/store/application/product/ProductService.java
+++
@@ -1,4 +0,0 @@
-package org.sparta.delivery.store.application.product;
-
-public class ProductService {
-}
