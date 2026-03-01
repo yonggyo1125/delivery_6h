44d5835f2d0cd20a97d1eede48624a14edfd6bb2
yonggyo1125 <yonggyo00@kakao.com>
2026-03-01 23:44:44 +0900

    .

--- src/main/java/org/sparta/delivery/global/infrastructure/swagger/SwaggerConfig.java
+++ src/main/java/org/sparta/delivery/global/infrastructure/swagger/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     @Bean
     public GroupedOpenApi userApi() {
         return GroupedOpenApi.builder()
-                .group("1. 회원 관리")
+                .group("회원 관리")
                 .displayName("회원 API")
                 .pathsToMatch("/v1/user/**")
                 .build();
@@ -25,7 +25,7 @@ public class SwaggerConfig {
     @Bean
     public GroupedOpenApi storeApi() {
         return GroupedOpenApi.builder()
-                .group("2. 매장 및 상품 관리")
+                .group("매장 및 상품 관리")
                 .displayName("매장/상품 API")
                 // 매장(/v1/stores)과 상품(/v1/stores/**/products)을 하나의 그룹으로 묶음
                 .pathsToMatch("/v1/stores/**")
@@ -35,7 +35,7 @@ public class SwaggerConfig {
     @Bean
     public GroupedOpenApi orderApi() {
         return GroupedOpenApi.builder()
-                .group("3. 주문 관리")
+                .group("주문 관리")
                 .displayName("주문 API")
                 .pathsToMatch("/v1/order/**")
                 .build();
---
+++ src/main/java/org/sparta/delivery/global/presentation/MainController.java
@@ -0,0 +1,12 @@
+package org.sparta.delivery.global.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/api-docs.html";
+    }
+}
