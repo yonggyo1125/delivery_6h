ad57295d4e5c82d9110d4b4ed6747427129177b5
yonggyo1125 <yonggyo00@kakao.com>
2026-03-07 22:11:22 +0900

    .

---
+++ src/main/resources/static/demo/toss/toss.js
@@ -0,0 +1,34 @@
+const clientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
+const customerKey = "USER_" + Math.random().toString(36).substring(2, 11);
+const paymentWidget = PaymentWidget(clientKey, customerKey);
+
+// 초기값 설정
+let amount = parseInt(document.getElementById("order-amount-input").value);
+document.getElementById("order-id-input").value = "ORDER_" + new Date().getTime();
+
+// 1. 결제 위젯 렌더링
+const paymentMethodsWidget = paymentWidget.renderPaymentMethods("#payment-method", { value: amount });
+paymentWidget.renderAgreement("#agreement");
+
+// 금액 수정 시 위젯 업데이트 버튼 이벤트
+document.getElementById("apply-btn").addEventListener("click", () => {
+    amount = parseInt(document.getElementById("order-amount-input").value);
+    paymentMethodsWidget.updateAmount(amount);
+    alert("결제 금액이 " + amount + "원으로 갱신되었습니다.");
+});
+
+// 결제하기 버튼 클릭
+document.getElementById("payment-button").addEventListener("click", () => {
+    const orderId = document.getElementById("order-id-input").value;
+    const orderName = document.getElementById("order-name-input").value;
+
+    paymentWidget.requestPayment({
+        orderId: orderId,
+        orderName: orderName,
+        customerName: "Yonggyo",
+        successUrl: window.location.origin + "/v1/payments/success",
+        failUrl: window.location.origin + "/v1/payments/fail",
+    }).catch(error => {
+        alert(error.message);
+    });
+});
